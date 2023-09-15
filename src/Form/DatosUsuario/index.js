b5c6c7cd06fc9445815584e0fba91085639c9b4b
Omar Augusto Bautista Mora <oabm77@gmail.com>
2023-09-15 09:19:47 -0500

    01 Entendiendo el problema - FunctionalComponents

--- src/Form/DatosUsuario/index.js
+++ src/Form/DatosUsuario/index.js
@@ -1,64 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button, Box } from "@mui/material";
 
-class DatosUsuario extends React.Component {
+const DatosUsuario = () => {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: {
-        value: '',
-        valid: true
-      },
-      password: {
-        value: '',
-        valid: true
-      }
-    }
-  }
+  const [email, setEmail] = useState({ value: "Omar@gg.com", valid: true })
+  const [password, setPassword] = useState({ value: "abc", valid: true })
 
-  render() {
-    return (
-      <Box
-        component="form"
-        autocomplete="off"
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "column",
-        }}
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log(this.state);
-        }}
-      >
-        <TextField
-          label="Correo electrónico"
-          variant="outlined"
-          fullWidth
-          margin="dense"
-          type="email"
-          error={false}
-          helperText={false && "Ingresa un correo electrónico válido"}
-          value={this.state.email.value}
-          onChange={(input) => this.setState({email: {value: input.target.value}})}
-        />
-        <TextField
-          label="Contraseña"
-          variant="outlined"
-          fullWidth
-          margin="dense"
-          type="password"
-          value={this.state.password.value}
-          onChange={(input) => this.setState({password: {value: input.target.value}})}
-        />
-        <Button variant="contained" type="submit">
-          Siguiente
-        </Button>
-      </Box>
-    );
-  }
+  return (
+    <Box
+      component="form"
+      autocomplete="off"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(email, password);
+      }}
+    >
+      <TextField
+        label="Correo electrónico"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="email"
+        error={false}
+        helperText={false && "Ingresa un correo electrónico válido"}
+        value={email.value}
+        onChange={(input) => setEmail({ value: input.target.value, valid: true })}
+      />
+      <TextField
+        label="Contraseña"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="password"
+        value={password.value}
+        onChange={(input) => setPassword({ password: { value: input.target.value, valid: true } })}
+      />
+      <Button variant="contained" type="submit">
+        Siguiente
+      </Button>
+    </Box>
+  );
 }
 
 export default DatosUsuario;
